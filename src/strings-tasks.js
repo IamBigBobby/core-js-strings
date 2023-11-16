66f0b85ff2e85aa2cdaece3e67f02df70994d5e9
Denis Svetleishii <denissvetleishii@outlook.com>
2023-11-16 15:50:18 +0400

    feat: add solution isPalindrome

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -342,10 +342,10 @@ function countVowels(/* str */) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const newStr = str.replace(/[^\w]/g, '').toLowerCase();
+  return newStr === newStr.split('').reverse().join('');
 }
-
 /**
  * Find the longest word in the sentence. If there are multiple longest words,
  * the function returns the first one encountered.
